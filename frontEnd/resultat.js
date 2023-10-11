d6c0e14874614e1f77878dc3dced7bd49978a9f4
MEUNIER Adrien <adrien.meunier42@gmail.com>
2023-10-11 16:57:33 +0200

    push result

--- frontEnd/resultat.js
+++ frontEnd/resultat.js
@@ -17,8 +17,38 @@ document.addEventListener("DOMContentLoaded", function () {
         const questionText = document.querySelector(".questionText");
 
         questionTitle.textContent = "Résultat";
-        typeAvecLePlusDeReponses ? questionText.innerHTML = `Selon vos réponses vous êtes témoin de <br>HARCELEMENT ${typeAvecLePlusDeReponses}<br><br>Cela représente  % des résultats` : questionText.innerHTML = `Selon vos réponses vous n'êtes pas témoin de harcelement`
-   } else {
+        if(typeAvecLePlusDeReponses){
+            questionText.innerHTML = `Selon vos réponses vous êtes témoin de <br>HARCELEMENT ${typeAvecLePlusDeReponses}<br><br>Cela représente  % des résultats`;
+        
+            const data = { type: typeAvecLePlusDeReponses };
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data) 
+            };
+            const url = 'http://172.168.1.120:3000/answers';
+            fetch(url, options)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Échec de la requête POST');
+                    }
+                })
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }else{
+            questionText.innerHTML = `Selon vos réponses vous n'êtes pas témoin de harcelement`;
+        }  
+   
+   
+    } else {
         console.log("Aucun résultat n'a été transmis.");
     }
 });
